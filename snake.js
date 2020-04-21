3ed1fd4a860bd330bdddaaa3023e82e4e322c29a
Máté Szabó <szabomati@gmail.com>
2020-04-21 17:00:25 +0200

    Move of the snake and some coloring

--- snake.js
+++ snake.js
@@ -1,19 +1,23 @@
-var spacer = 1;
+var spacer = 2;
 var x = 0;
-var speed = 40;
+var speed = 15;
 var snake_body = [];
+var direction;
 
 function setup() {
     createCanvas(500, 600);
     background(0);
     var body1 = new Square(width / 2, height / 2);
+    body1.color = color(181, 118, 0);
     var body2 = new Square(width / 2, body1.y + body1.width + spacer);
     var body3 = new Square(width / 2, body2.y + body2.width + spacer);
     snake_body.push(body1, body2, body3);
+    direction = "up";
 }
 
 function draw() {
     if (frameCount % speed == 0) {
+        moveSnake();
         drawSnake();
     }
 }
@@ -23,7 +27,77 @@ function drawSnake() {
     createCanvas(500, 600);
     background(0);
     for (var i = 0; i < snake_body.length; i++) {
+        fill(snake_body[i].color);
+        noStroke();
         square(snake_body[i].x, snake_body[i].y, snake_body[i].width);
+    }
+}
 
+function moveSnake() {
+    if (direction == "up") {
+        for (var i = snake_body.length - 1; i >= 0; i--) {
+            if (i == 0) {
+                snake_body[i].x = snake_body[i].x;
+                snake_body[i].y = snake_body[i].y - (snake_body[i].width + spacer);
+            }
+            else {
+                snake_body[i].x = snake_body[i - 1].x;
+                snake_body[i].y = snake_body[i - 1].y;
+            }
+        }
+    }
+    if (direction == "down") {
+        for (var i = snake_body.length - 1; i >= 0; i--) {
+            if (i == 0) {
+                snake_body[i].x = snake_body[i].x;
+                snake_body[i].y = snake_body[i].y + (snake_body[i].width + spacer);
+            }
+            else {
+                snake_body[i].x = snake_body[i - 1].x;
+                snake_body[i].y = snake_body[i - 1].y;
+            }
+        }
+    }
+    if (direction == "right") {
+        for (var i = snake_body.length - 1; i >= 0; i--) {
+            if (i == 0) {
+                snake_body[i].x = snake_body[i].x + (snake_body[i].width + spacer);
+                snake_body[i].y = snake_body[i].y;
+            }
+            else {
+                snake_body[i].x = snake_body[i - 1].x;
+                snake_body[i].y = snake_body[i - 1].y;
+            }
+        }
+    }
+    if (direction == "left") {
+        for (var i = snake_body.length - 1; i >= 0; i--) {
+            if (i == 0) {
+                snake_body[i].x = snake_body[i].x - (snake_body[i].width + spacer);
+                snake_body[i].y = snake_body[i].y;
+            }
+            else {
+                snake_body[i].x = snake_body[i - 1].x;
+                snake_body[i].y = snake_body[i - 1].y;
+            }
+        }
+    }
+}
+
+function keyPressed() {
+    if (keyCode == LEFT_ARROW && direction != "right") {
+        direction = "left";
+    }
+
+    if (keyCode == UP_ARROW && direction != "down") {
+        direction = "up";
+    }
+
+    if (keyCode == RIGHT_ARROW && direction != "left") {
+        direction = "right";
+    }
+
+    if (keyCode == DOWN_ARROW && direction != "up") {
+        direction = "down";
     }
 }
--- square.js
+++ square.js
@@ -2,6 +2,7 @@ class Square {
     x;
     y;
     width = 10;
+    color = color(33, 105, 0);
 
     constructor(x, y) {
         this.x = x;
