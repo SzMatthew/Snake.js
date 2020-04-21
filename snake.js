99ad57a4d90af028d766cad3420ae91721eb9800
Máté Szabó <szabomati@gmail.com>
2020-04-21 11:29:11 +0200

    Square class, Draw of default Snake

--- snake.js
+++ snake.js
@@ -1,10 +1,29 @@
-function setup()
-{
+var spacer = 1;
+var x = 0;
+var speed = 40;
+var snake_body = [];
+
+function setup() {
     createCanvas(500, 600);
     background(0);
+    var body1 = new Square(width / 2, height / 2);
+    var body2 = new Square(width / 2, body1.y + body1.width + spacer);
+    var body3 = new Square(width / 2, body2.y + body2.width + spacer);
+    snake_body.push(body1, body2, body3);
 }
 
-function draw()
-{
+function draw() {
+    if (frameCount % speed == 0) {
+        drawSnake();
+    }
+}
 
+function drawSnake() {
+    clear();
+    createCanvas(500, 600);
+    background(0);
+    for (var i = 0; i < snake_body.length; i++) {
+        square(snake_body[i].x, snake_body[i].y, snake_body[i].width);
+
+    }
 }
---
+++ square.js
@@ -0,0 +1,10 @@
+class Square {
+    x;
+    y;
+    width = 10;
+
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
